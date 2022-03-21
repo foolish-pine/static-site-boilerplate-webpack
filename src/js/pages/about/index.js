d075beb1dc1bf0f617ed0b2b7acd62c3d1146ade
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-21 23:06:56 +0900

    [update] scssのフォルダ構成を変更

--- src/js/pages/about/index.js
+++ src/js/pages/about/index.js
@@ -1,3 +1,2 @@
 import "@/css/vendor/reset/html5reset-1.6.1.css";
-import "@/scss/common/index.scss";
 import "@/scss/pages/about/style.scss";
--- src/js/pages/top/index.js
+++ src/js/pages/top/index.js
@@ -1,5 +1,4 @@
 import "@/css/vendor/reset/html5reset-1.6.1.css";
-import "@/scss/common/index.scss";
 import "@/scss/pages/top/style.scss";
 
 import { hello } from "@/js/components/hello";
